af16ae107f71a23e8e2ceac0f37161c192cf960a
Zesande Mbekwa <zesandembekwa8@gmail.com>
2026-03-29 02:27:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int guess;
+            int number;
+            number = random.Next(1, 10);
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (number == guess)
+                {
+                    Console.WriteLine("Horrey you won!");
+                }
+                else
+                {
+                    Console.WriteLine($"You loose! the number is {number}");
+                }
+            } while (number != guess);
+        }
+    }
+}
